ed81d890bb5070158ab86a5ab2b652dd4d92e3eb
panda <seruy722@gmail.com>
2021-03-25 11:45:41 +0200

    fix bug

--- client/src/pages/Drafts.vue
+++ client/src/pages/Drafts.vue
@@ -326,7 +326,7 @@ export default {
             const url = window.URL.createObjectURL(new Blob([response.data]));
             const link = document.createElement('a');
             link.href = url;
-            link.setAttribute('download', 'brands.xlsx');
+            link.setAttribute('download', 'олаченные места.xlsx');
             document.body.appendChild(link);
             link.click();
           } else {
