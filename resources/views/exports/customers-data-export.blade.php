4fee89976e056985370abea11a47718aa9c2ab0e
Sergey Denisiuk <seruy722@gmail.com>
2022-01-31 09:19:59 +0200

    update depp

--- resources/views/exports/customers-data-export.blade.php
+++ resources/views/exports/customers-data-export.blade.php
@@ -5,10 +5,10 @@
         <th style="font-weight:bold;border: 1px solid black;text-align: center;">Им</th>
         <th style="font-weight:bold;border: 1px solid black;text-align: center;">Тел</th>
         <th style="font-weight:bold;border: 1px solid black;text-align: center;">Гор</th>
-        <th style="font-weight:bold;border: 1px solid black;text-align: center;">Сп. доставки</th>
-        <th style="font-weight:bold;border: 1px solid black;text-align: center;">Отд</th>
-        <th style="font-weight:bold;border: 1px solid black;text-align: center;">Пол</th>
-        <th style="font-weight:bold;border: 1px solid black;text-align: center;">Кол кг</th>
+{{--        <th style="font-weight:bold;border: 1px solid black;text-align: center;">Сп. доставки</th>--}}
+{{--        <th style="font-weight:bold;border: 1px solid black;text-align: center;">Отд</th>--}}
+{{--        <th style="font-weight:bold;border: 1px solid black;text-align: center;">Пол</th>--}}
+{{--        <th style="font-weight:bold;border: 1px solid black;text-align: center;">Кол кг</th>--}}
     </tr>
     </thead>
     <tbody>
@@ -18,10 +18,10 @@
             <td style="border: 1px solid black;text-align: center;">{{ $elem->name }}</td>
             <td style="border: 1px solid black;text-align: center;">{{ $elem->phone }}</td>
             <td style="border: 1px solid black;text-align: center;">{{ $elem->city_name }}</td>
-            <td style="border: 1px solid black;text-align: center;">{{ $elem->delivery_method_name }}</td>
-            <td style="border: 1px solid black;text-align: center;">{{ $elem->department }}</td>
-            <td style="border: 1px solid black;text-align: center;">{{ $elem->sex }}</td>
-            <td style="border: 1px solid black;text-align: center;">{{ $elem->count }}</td>
+{{--            <td style="border: 1px solid black;text-align: center;">{{ $elem->delivery_method_name }}</td>--}}
+{{--            <td style="border: 1px solid black;text-align: center;">{{ $elem->department }}</td>--}}
+{{--            <td style="border: 1px solid black;text-align: center;">{{ $elem->sex }}</td>--}}
+{{--            <td style="border: 1px solid black;text-align: center;">{{ $elem->count }}</td>--}}
         </tr>
     @endforeach
     </tbody>
