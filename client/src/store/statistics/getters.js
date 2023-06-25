8b0642836c29353a84d5801b00b301517e0e95c8
Sergey Denisiuk <seruy722@gmail.com>
2023-06-25 20:32:39 +0300

    update depp and fix profitsum

--- client/src/store/statistics/getters.js
+++ client/src/store/statistics/getters.js
@@ -2,9 +2,10 @@ export const getStatistics = (state) => {
   const arr = [];
   _.forEach(state.statistics.month, (item) => {
     const currentMonth = new Date(item.date).getMonth() + 1;
+    const currentYear = new Date(item.date).getFullYear();
     const obj = _.assign({}, item);
     obj.date = new Date(item.date).toISOString().slice(0, 7);
-    obj.data = _.orderBy(_.filter(state.statistics.statistics, (elem) => currentMonth === new Date(elem.created_at).getMonth() + 1), (elem) => new Date(elem.created_at), 'desc');
+    obj.data = _.orderBy(_.filter(state.statistics.statistics, (elem) => currentMonth === new Date(elem.created_at).getMonth() + 1 && currentYear === new Date(elem.created_at).getFullYear()), (elem) => new Date(elem.created_at), 'desc');
     arr.push(obj);
   });
   return arr;
